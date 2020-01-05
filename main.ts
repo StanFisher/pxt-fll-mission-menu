be5e56b45bc7181ec75f8517d32b6462675ded71
Stan Fisher <stanley.fisher@gmail.com>
2020-01-05 15:28:51 -0600

    modified API to add a mission

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ const missionMenu = new MissionMenu();
 
 //% weight=100 color=#007BA7 icon="\uf0c9"
 namespace Menu {
-    //% block="on $displayName selected to run"
+    //% block="define mission $displayName"
     export function addMission(displayName: string, missionFunction: () => void): void {
         const mission: IMission = {
             displayName: displayName,
